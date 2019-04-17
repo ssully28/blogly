ce85c0ef5aedf420e07ad03ba08e1928217b567b
Jason Matthias <jpmatthias90@gmail.com>
2019-04-17 16:15:07 -0700

    added tests for create, delete users and see user profile

--- test.py
+++ test.py
@@ -1,8 +1,30 @@
 
 from app import app
 from unittest import TestCase
+from models import db, connect_db, User
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly_test'
+connect_db(app)
+db.create_all()
+User.query.delete()
+
+# Add test-users to blogly_test
+whiskey = User(user_name='whisky-test', first_name='Whiskey', last_name="dog")
+bowser = User(user_name='bowser-test', first_name='Bowser', last_name="dog")
+spike = User(user_name='spike-test', first_name='Spike', last_name="porcupine")
+
+# Add new objects to session, so they'll persist
+db.session.add(whiskey)
+db.session.add(bowser)
+db.session.add(spike)
+
+# Commit--otherwise, this never gets saved!
+db.session.commit()
+
+
 
 class FlaskTests(TestCase):
+
 
     def setUp(self):
         self.client = app.test_client()
@@ -11,16 +33,41 @@ class FlaskTests(TestCase):
         with self.client:
             result = self.client.get('/users')
             self.assertEqual(result.status_code, 200)
-            self.assertIn(b'<h1>Users</h1>', result.data)
+            self.assertIn(b'<h1 class="col-2">Users</h1>', result.data)
 
     def test_root(self):
         with self.client:
             result = self.client.get('/', follow_redirects=True)
             self.assertEqual(result.status_code, 200)
-            self.assertIn(b'<h1>Users</h1>', result.data)
+            self.assertIn(b'<h1 class="col-2">Users</h1>', result.data)
 
     def test_add_form(self):
         with self.client:
             result = self.client.get('/users/new')
             self.assertEqual(result.status_code, 200)
-            self.assertIn(b'<h1>Create a user</h1>', result.data)
+            self.assertIn(b'<h1 class="display-5 mt-4">Create a user</h1>', result.data)
+
+    def test_user_profile(self):
+        with self.client:
+            result = self.client.get('/users/whisky-test')
+            self.assertEqual(result.status_code, 200)
+            self.assertIn(b'<h1 class="display-4">\n            whisky-test\n        </h1>', result.data)
+
+    def test_add_user(self):
+        with self.client:
+            result = self.client.post('/users', data={
+                "user-name": "cool-guy-johnny-B",
+                "first-name": "johnny",
+                "last-name": "bravo",
+                "image-url": "https://static.independent.co.uk/s3fs-public/thumbnails/image/2018/06/25/14/cat-lizard.jpg"
+            }, follow_redirects=True)
+            self.assertEqual(result.status_code, 200)
+            self.assertIn(b'<h5 class="card-title">cool-guy-johnny-B</h5>',
+                                        result.data)
+    
+    def test_delete_user(self):
+        with self.client:
+            result = self.client.post('/users/bowser-test/delete',
+                                        follow_redirects=True)
+            self.assertEqual(result.status_code, 200)
+            self.assertNotIn(b'bowser-test', result.data)
